3bd62a2548d9dfbfc5993241fc1ade0f1b4b260b
Mariana Lourenço <marianaisabel@student.dei.uc.pt>
2022-04-24 17:32:06 +0100

    pagina 404 not found

--- marianaisabel-react-proj5/src/pages/ErrorPage404.js
+++ marianaisabel-react-proj5/src/pages/ErrorPage404.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import { FormattedMessage ,useIntl} from "react-intl";
+
+import {
+    Flex,
+} from "@chakra-ui/react";
+
+
 function ErrorPage404() {
   return (
-    <div>ErrorPage404</div>
+    <Flex
+    flexDirection="column"
+    width="100wh"
+    minHeight="83vh"
+    backgroundColor="gray.200"
+    justifyContent="center"
+    alignItems="center"
+    >
+      <FormattedMessage id={"error_pageNotFound_404"}></FormattedMessage>
+   
+    </Flex>
   )
 }
 
