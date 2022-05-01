5e443cbdec51deedd853eab5ba5b7061d62d9d83
Mariana Lourenço <marianaisabel@student.dei.uc.pt>
2022-05-01 04:58:09 +0100

    lista de utilizadores associados ao projecto na vista do projecto

--- marianaisabel-react-proj5/src/components/sections/Project/AssocUsersTab.js
+++ marianaisabel-react-proj5/src/components/sections/Project/AssocUsersTab.js
@@ -49,10 +49,60 @@ import { ExternalLinkIcon, CheckIcon, CloseIcon, EditIcon, DeleteIcon,WarningTwo
 import useFetch from 'use-http';
 import { connect } from 'react-redux'
 import EditableControls from "../../EditableControls"
-
-const AssocUsersTab=({isAdmin, currentProject,...props})=>{
+//TODO: 
+function setAppError(error){
+    console.log(error)
+}
+const AssocUsersTab=({isAdmin,...props})=>{
     const navigate = useNavigate();
     const intl = useIntl();
+
+
+    const { get, post, del, response, loading, error } = useFetch();
+
+    //const [currentId, setCurrentId]=useState("");
+    const { id } = useParams();
+    console.log(id)
+   // setCurrentId(id)
+
+
+
+        /**** *******************************************STATE******************************************************** */
+  
+        const [users, setUsers]=useState([])
+        const [usersOnProject, setUsersOnProject]=useState([])
+        const [restResponse, setRestResponse]=useState(""); //OK or NOK or ""
+    
+    /**** ******************************************USE EFFECT********************************************************* */
+ 
+    useEffect(async()=>{
+        console.log("dentro do userEffect");
+        setRestResponse("");
+
+        const usersOnProject = await get("projects/"+id+"/assocUsersList")
+        if (response.ok) {
+            console.log(usersOnProject)
+            setUsersOnProject(usersOnProject);
+            setAppError("");
+        } else if(response.status==401) {
+            console.log("credenciais erradas? " + error)
+            setAppError('error_fetch_login_401');
+            setRestResponse("NOK");
+        }else{
+            console.log("houve um erro no fetch " + error)
+            if(error && error!=""){
+                setAppError(  error );
+                setRestResponse("NOK");
+            }else{
+                setAppError(  "error_fetch_generic" );
+                setRestResponse("NOK");
+            }
+        }
+
+        
+    },[])
+
+
     return (<>                
             <Heading as="h3" ><FormattedMessage id={"associated_users"} />:</Heading>
 
@@ -62,10 +112,11 @@ const AssocUsersTab=({isAdmin, currentProject,...props})=>{
                 <Tr>
                     <Th>{intl.formatMessage({id: 'user_name'})}</Th>
                     <Th>{intl.formatMessage({id: 'form_field_email'})}</Th>
+                    <Th>{intl.formatMessage({id: 'job'})}</Th>
                 </Tr>
                 </Thead>
                 <Tbody>
-                {currentProject.associatedUsers.map((u)=>(
+                {usersOnProject.map((u)=>(
                     <Tr key={u.id}>
                     <Td color="gray.500" fontWeight="400" >{u.firstName + " " + u.lastName}</Td>
                     <Td color="gray.500" fontWeight="400" >{u.email}</Td>
